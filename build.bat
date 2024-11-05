adf29bc555efbcec8e5896bbecc1c607c46d330c
jorgtunubala <jorgetunubala@unicauca.edu.co>
2024-11-04 20:38:42 -0500

    Ajustes para agregar seguridad al microservicio utilizando spring security

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+set SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/maestriacomputacion
+set SPRING_DATASOURCE_USERNAME=root
+set SPRING_DATASOURCE_PASSWORD=mysql
+set SPRING_MAIL_SMTP=smtp.gmail.com
+set SPRING_MAIL_PORT=587
+set SPRING_MAIL_USERNAME=maestriaprueba2024@gmail.com
+set SPRING_MAIL_PASSWORD=qawl sbgr mawn dzkm
+set SPRING_SERVER_SOLICITUDES_PORT=8095
+set SPRING_OAUTH2_JWT_URI=https://dev-d0qrbj0szt6vmqvq.us.auth0.com/
+set SPRING_OAUTH2_JWT_AUDIENCE=https://maestriacomputacion.com
+
+mvn clean package
---
+++ src/main/java/com/maestria/gestionSolicitudes/comun/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.maestria.gestionSolicitudes.comun.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors() // Habilitar CORS
+            .and()
+            .authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer()
+                .jwt();
+    }
+
+    // Define un bean de CORS para configuración personalizada
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
